224b15331af75be66dde4c394f05da6f8342ca0d
lhs <424447025@qq.com>
2025-09-26 14:20:50 +0800

    在UI层中的UserContrtoller中添加Model和BLL层的引用，添加Register的POST请求的方法

--- recycling.Web.UI/Controllers/UserController.cs
+++ recycling.Web.UI/Controllers/UserController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using recycling.BLL;
+using recycling.Model;
 
 namespace recycling.Web.UI.Controllers
 {
     public class UserController : Controller
     {
+        private UserBLL _userBLL = new UserBLL();
         // GET: User
         public ActionResult Index()
         {
@@ -28,11 +31,39 @@ namespace recycling.Web.UI.Controllers
         {
             ViewBag.LoginType = "email";
             return View("Login");
-        }
+        }   
         public ActionResult Register()
         {
             return View();
         }
+
+        // POST: User/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // 调用BLL层进行注册验证和处理
+                string errorMessage = _userBLL.Register(model);
+
+                if (string.IsNullOrEmpty(errorMessage))
+                {
+                    // 注册成功，跳转到登录页并显示成功信息
+                    TempData["SuccessMessage"] = "注册成功，请登录";
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    // 注册失败，显示错误信息（按优先级）
+                    ModelState.AddModelError("", errorMessage);
+                }
+            }
+
+            // 验证失败，返回注册页面
+            return View(model);
+        }
+
         public ActionResult Forgot()
         {
             return View();
