0cef90439e39a8d2de73f810bbee10e0962eadd2
lhs <424447025@qq.com>
2025-09-28 17:18:00 +0800

    修改Model层中的ForgotPasswordViewModel类的代码

--- recycling.Model/ForgotPasswordViewModel.cs
+++ recycling.Model/ForgotPasswordViewModel.cs
@@ -12,42 +12,24 @@ namespace recycling.Model
     /// </summary>
     public class ForgotPasswordViewModel
     {
-        /// <summary>
-        /// 手机号
-        /// </summary>
-        [Required]
+        [Required(ErrorMessage = "手机号不能为空")]
+        [RegularExpression(@"^1[3-9]\d{9}$", ErrorMessage = "请输入有效的手机号")]
         [Display(Name = "手机号")]
         public string PhoneNumber { get; set; }
 
-        /// <summary>
-        /// 验证码
-        /// </summary>
-        [Required]
+        [Required(ErrorMessage = "验证码不能为空")]
         [Display(Name = "验证码")]
         public string VerificationCode { get; set; }
 
-        /// <summary>
-        /// 新密码
-        /// </summary>
-        [Required]
+        [Required(ErrorMessage = "新密码不能为空")]
+        [DataType(DataType.Password)]
         [Display(Name = "新密码")]
         public string NewPassword { get; set; }
 
-        /// <summary>
-        /// 确认新密码
-        /// </summary>
-        [Required]
+        [Required(ErrorMessage = "请确认新密码")]
+        [Compare("NewPassword", ErrorMessage = "两次输入的密码不一致")]
+        [DataType(DataType.Password)]
         [Display(Name = "确认新密码")]
         public string ConfirmNewPassword { get; set; }
-    }
-
-    /// <summary>
-    /// 验证码存储模型（用于服务器端临时存储验证码）
-    /// </summary>
-    public class VerificationCodeModel
-    {
-        public string PhoneNumber { get; set; }
-        public string Code { get; set; }
-        public DateTime ExpireTime { get; set; }
     }
 }
