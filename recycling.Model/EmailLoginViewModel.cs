524d288baaed25c7d4893ae584f082ee8df45b0f
lhs <424447025@qq.com>
2025-09-29 10:21:18 +0800

    在Model层给EmailLoginViewModel类添加对应需求的设计代码

--- recycling.Model/EmailLoginViewModel.cs
+++ recycling.Model/EmailLoginViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace recycling.Model
 {
-    class EmailLoginViewModel
+    /// <summary>
+    /// 邮箱登录视图模型
+    /// </summary>
+    public class EmailLoginViewModel
     {
+        [Required(ErrorMessage = "邮箱不能为空")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[^\s]+@[^\s]+\.[^\s]+$", ErrorMessage = "请输入有效的邮箱地址")]
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "验证码不能为空")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "验证码必须为6位")]
+        [Display(Name = "验证码")]
+        public string VerificationCode { get; set; }
     }
 }
