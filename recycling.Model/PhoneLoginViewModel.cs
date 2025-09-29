6d023d4edd67494a353d371a38aee6d2cc5fe20b
lhs <424447025@qq.com>
2025-09-29 09:40:46 +0800

    在Model层中给新增加的PhoneLoginViewModel类添加对应的设计代码

--- recycling.Model/PhoneLoginViewModel.cs
+++ recycling.Model/PhoneLoginViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace recycling.Model
 {
-    class PhoneLoginViewModel
+    /// <summary>
+    /// 手机号登录视图模型
+    /// </summary>
+    public class PhoneLoginViewModel
     {
+        [Required(ErrorMessage = "手机号不能为空")]
+        [RegularExpression(@"^1[3-9]\d{9}$", ErrorMessage = "请输入有效的手机号")]
+        [Display(Name = "手机号")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "验证码不能为空")]
+        [Display(Name = "验证码")]
+        public string VerificationCode { get; set; }
     }
 }
