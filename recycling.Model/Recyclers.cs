3c8ae39b9bcc0f7529b4e97ad38178518cb93d4d
lhs <424447025@qq.com>
2025-09-30 15:10:31 +0800

    在Model层中添加Recyclers的实体框架，并且删除了User类

---
+++ recycling.Model/Recyclers.cs
@@ -0,0 +1,46 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Recyclers
+    {
+        [Key]
+        public int RecyclerID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string PasswordHash { get; set; }
+
+        public bool Available { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Region { get; set; }
+
+        public decimal? Rating { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [StringLength(255)]
+        public string AvatarURL { get; set; }
+    }
+}
