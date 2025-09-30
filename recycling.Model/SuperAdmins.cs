0edbf183c37747a9a471cd15c54ef4aeb9bdac12
lhs <424447025@qq.com>
2025-09-30 15:17:40 +0800

    在Model层中添加Admins和SuperAdmins管理员和超级管理员的实体框架

---
+++ recycling.Model/SuperAdmins.cs
@@ -0,0 +1,32 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SuperAdmins
+    {
+        [Key]
+        public int SuperAdminID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
