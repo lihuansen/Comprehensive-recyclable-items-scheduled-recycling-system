b6361591f6dae0ad566a5ea8dde290d164e53733
lhs <424447025@qq.com>
2025-10-20 16:08:41 +0800

    在Model层中给ChangePasswordViewModel类添加设计代码

--- recycling.Model/ChangePasswordViewModel.cs
+++ recycling.Model/ChangePasswordViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace recycling.Model
 {
-    class ChangePasswordViewModel
+    public class ChangePasswordViewModel
     {
+        [Required(ErrorMessage = "请输入当前密码")]
+        [DataType(DataType.Password)]
+        [Display(Name = "当前密码")]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "请输入新密码")]
+        [DataType(DataType.Password)]
+        [Display(Name = "新密码")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "请确认新密码")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "两次输入的密码不一致")]
+        [Display(Name = "确认新密码")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
