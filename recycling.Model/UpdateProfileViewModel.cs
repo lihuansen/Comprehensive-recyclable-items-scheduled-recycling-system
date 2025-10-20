8a72b67394a1a8b5fa24de4201a5e689122a5831
lhs <424447025@qq.com>
2025-10-20 16:05:38 +0800

    在Model层中给UpdateProfileViewModel类添加设计代码

--- recycling.Model/UpdateProfileViewModel.cs
+++ recycling.Model/UpdateProfileViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace recycling.Model
 {
-    class UpdateProfileViewModel
+    public class UpdateProfileViewModel
     {
+        [Required(ErrorMessage = "用户名不能为空")]
+        [StringLength(50, ErrorMessage = "用户名长度不能超过50个字符")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "手机号不能为空")]
+        [RegularExpression(@"^1[3-9]\d{9}$", ErrorMessage = "请输入有效的手机号")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "邮箱不能为空")]
+        [EmailAddress(ErrorMessage = "请输入正确的邮箱格式")]
+        public string Email { get; set; }
     }
 }
