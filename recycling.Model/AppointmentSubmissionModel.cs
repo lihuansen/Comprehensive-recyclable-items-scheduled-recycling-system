0896e8f33895915d97ad6a991cfb2392a5385e7d
lhs <424447025@qq.com>
2025-10-20 17:43:11 +0800

    在Model层中添加AppointmentSubmissionModel类，并且添加对应的设计代码

---
+++ recycling.Model/AppointmentSubmissionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recycling.Model
+{
+    public class AppointmentSubmissionModel
+    {
+        public AppointmentViewModel BasicInfo { get; set; }
+        public Dictionary<string, Dictionary<string, string>> CategoryAnswers { get; set; }
+        public decimal FinalPrice { get; set; }
+    }
+}
