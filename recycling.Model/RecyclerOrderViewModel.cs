c8b91a4c8d6ee65a4616e25bfa3a3800d27aaa49
lhs <424447025@qq.com>
2025-10-24 12:04:22 +0800

    添加设计代码至Model层中的RecyclerOrderViewModel类中

--- recycling.Model/RecyclerOrderViewModel.cs
+++ recycling.Model/RecyclerOrderViewModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace recycling.Model
 {
-    class RecyclerOrderViewModel
+    public class RecyclerOrderViewModel
     {
+        public int AppointmentID { get; set; }
+        public string OrderNumber { get; set; }
+        public string AppointmentType { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string TimeSlot { get; set; }
+        public decimal EstimatedWeight { get; set; }
+        public bool IsUrgent { get; set; }
+        public string Address { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CategoryNames { get; set; }
+        public int? RecyclerID { get; set; }
+        public string RecyclerName { get; set; }
     }
 }
