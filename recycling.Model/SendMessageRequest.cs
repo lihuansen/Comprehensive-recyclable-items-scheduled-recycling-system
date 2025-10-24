d9b17a3876621011fab0016f9f67954c1d17180e
lhs <424447025@qq.com>
2025-10-24 12:09:06 +0800

    添加设计代码至Model层中的SendMessageRequest类中

--- recycling.Model/SendMessageRequest.cs
+++ recycling.Model/SendMessageRequest.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace recycling.Model
 {
-    class SendMessageRequest
+    public class SendMessageRequest
     {
+        public int OrderID { get; set; }
+        public string SenderType { get; set; }
+        public int SenderID { get; set; }
+        public string Content { get; set; }
     }
 }
