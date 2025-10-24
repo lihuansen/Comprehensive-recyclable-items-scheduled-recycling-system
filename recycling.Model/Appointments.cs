d512dd8964f8f2e619056aa3b553c651ee60ca8a
lhs <424447025@qq.com>
2025-10-24 11:43:58 +0800

    由于修改了数据库，所以前面进行了删除后，现在重新添加Appointments类到Model层中

---
+++ recycling.Model/Appointments.cs
@@ -0,0 +1,60 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Appointments
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string AppointmentType { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TimeSlot { get; set; }
+
+        public decimal EstimatedWeight { get; set; }
+
+        public bool IsUrgent { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ContactPhone { get; set; }
+
+        [StringLength(500)]
+        public string SpecialInstructions { get; set; }
+
+        public decimal? EstimatedPrice { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Status { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime CreatedDate { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime UpdatedDate { get; set; }
+
+        public int? RecyclerID { get; set; }
+    }
+}
