54671c5f20f152817c1fe7da764d0595758fd76e
lhs <424447025@qq.com>
2025-10-24 12:07:11 +0800

    添加设计代码至Model层中的OrderFilterModel类中

--- recycling.Model/OrderFilterModel.cs
+++ recycling.Model/OrderFilterModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace recycling.Model
 {
-    class OrderFilterModel
+    public class OrderFilterModel
     {
+        public string OrderNumber { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public bool? IsUrgent { get; set; }
+        public string Status { get; set; }
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
