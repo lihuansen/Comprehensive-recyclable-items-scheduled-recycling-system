3c2d8ebe25b4e3d956d3ed2deee1ba6890f3f4d1
lhs <424447025@qq.com>
2025-10-27 14:06:45 +0800

    在Model层中的OrderDetailModel类中添加设计代码

--- recycling.Model/OrderDetailModel.cs
+++ recycling.Model/OrderDetailModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace recycling.Model
 {
-    class OrderDetailModel
+    public class OrderDetailModel
     {
+        public string OrderNumber { get; set; }
+        public string AppointmentType { get; set; }
+        public string AppointmentDate { get; set; }
+        public string TimeSlot { get; set; }
+        public decimal EstimatedWeight { get; set; }
+        public decimal? EstimatedPrice { get; set; }
+        public bool IsUrgent { get; set; }
+        public string Address { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string Status { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+        public string SpecialInstructions { get; set; }
+        public string CategoryNames { get; set; }
     }
 }
