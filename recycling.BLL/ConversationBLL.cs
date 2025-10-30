6dd88882ee4edb8da791859f3e6f162a482354ac
lhs <424447025@qq.com>
2025-10-30 11:29:43 +0800

    在BLL层中给ConversationBLL类添加设计代码

--- recycling.BLL/ConversationBLL.cs
+++ recycling.BLL/ConversationBLL.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using recycling.DAL;
+using recycling.Model;
 
 namespace recycling.BLL
 {
-    class ConversationBLL
+    public class ConversationBLL
     {
+        private readonly ConversationDAL _conversationDAL = new ConversationDAL();
+
+        public bool EndConversation(int orderId, int userId)
+        {
+            if (orderId <= 0 || userId <= 0) return false;
+            return _conversationDAL.EndConversation(orderId, userId);
+        }
+
+        public List<ConversationViewModel> GetUserConversations(int userId, int pageIndex = 1, int pageSize = 50)
+        {
+            if (userId <= 0) return new List<ConversationViewModel>();
+            return _conversationDAL.GetUserConversations(userId, pageIndex, pageSize);
+        }
+
+        public List<ConversationViewModel> GetRecyclerConversations(int recyclerId, int pageIndex = 1, int pageSize = 50)
+        {
+            if (recyclerId <= 0) return new List<ConversationViewModel>();
+            return _conversationDAL.GetRecyclerConversations(recyclerId, pageIndex, pageSize);
+        }
+
+        public List<Messages> GetConversationMessagesBeforeEnd(int orderId, DateTime endedTime)
+        {
+            if (orderId <= 0 || endedTime == default(DateTime)) return new List<Messages>();
+            return _conversationDAL.GetConversationMessagesBeforeEnd(orderId, endedTime);
+        }
     }
 }
