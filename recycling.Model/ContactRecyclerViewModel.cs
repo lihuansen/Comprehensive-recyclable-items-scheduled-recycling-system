960eca0c104a9af85b99499206caabadfe1bde88
lhs <424447025@qq.com>
2025-10-31 17:02:35 +0800

    在Model层中给ContactRecyclerViewModel类添加对应的属性的代码

--- recycling.Model/ContactRecyclerViewModel.cs
+++ recycling.Model/ContactRecyclerViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace recycling.Model
 {
-    class ContactRecyclerViewModel
+    public class ContactRecyclerViewModel
     {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string RecyclerName { get; set; }
+        public string RecyclerPhone { get; set; }
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+
+        [Required(ErrorMessage = "请输入消息内容")]
+        [StringLength(500, ErrorMessage = "消息内容不能超过500个字符")]
+        public string MessageContent { get; set; }
     }
 }
