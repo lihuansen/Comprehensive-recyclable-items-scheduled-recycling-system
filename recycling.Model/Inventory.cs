e20c71de6071466a4c7c95fe6c19f40333d673b0
lhs <424447025@qq.com>
2026-01-08 12:15:32 +0800

    在Model层中添加新的Inventory实体类

---
+++ recycling.Model/Inventory.cs
@@ -0,0 +1,37 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Inventory")]
+    public partial class Inventory
+    {
+        public int InventoryID { get; set; }
+
+        public int OrderID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryKey { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int RecyclerID { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime CreatedDate { get; set; }
+
+        public decimal? Price { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string InventoryType { get; set; } = "StoragePoint";
+    }
+}
