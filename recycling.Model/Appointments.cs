329b06de91bcf7f458f31d438feb3f7e06cf2610
lhs <424447025@qq.com>
2026-03-05 15:24:15 +0800

    在Model层中添加Appointments实体类

---
+++ recycling.Model/Appointments.cs
@@ -0,0 +1,53 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Appointments
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+
+        public int? UserID { get; set; }
+
+        [StringLength(50)]
+        public string AppointmentType { get; set; }
+
+        public DateTime? AppointmentDate { get; set; }
+
+        [StringLength(50)]
+        public string TimeSlot { get; set; }
+
+        public decimal? EstimatedWeight { get; set; }
+
+        public bool? IsUrgent { get; set; }
+
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string ContactName { get; set; }
+
+        [StringLength(50)]
+        public string ContactPhone { get; set; }
+
+        public string SpecialInstructions { get; set; }
+
+        public decimal? EstimatedPrice { get; set; }
+
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public int? RecyclerID { get; set; }
+
+        public string RollbackReason { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+}
