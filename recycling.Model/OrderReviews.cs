7e81a2ce3df5d931766449d73615dda513d1cebe
lhs <424447025@qq.com>
2026-03-06 10:31:25 +0800

    在Model层中添加OrderReviews实体类

---
+++ recycling.Model/OrderReviews.cs
@@ -0,0 +1,28 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class OrderReviews
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        public int? OrderID { get; set; }
+
+        public int? UserID { get; set; }
+
+        public int? RecyclerID { get; set; }
+
+        public int? StarRating { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+        public string PictureUrl { get; set; }
+    }
+}
