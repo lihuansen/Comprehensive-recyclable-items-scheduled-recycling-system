1f1346d3101ed579f4f52e0595864de504f94578
lhs <424447025@qq.com>
2026-03-10 15:12:10 +0800

    在Model层中添加新设计的SortingCenterWorkers实体类

---
+++ recycling.Model/SortingCenterWorkers.cs
@@ -0,0 +1,56 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SortingCenterWorkers
+    {
+        [Key]
+        public int WorkerID { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FullName { get; set; }
+
+        [StringLength(50)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(50)]
+        public string IDNumber { get; set; }
+
+        public int? SortingCenterID { get; set; }
+
+        public string SortingCenterName { get; set; }
+
+        public bool? Available { get; set; }
+
+        [StringLength(50)]
+        public string CurrentStatus { get; set; }
+
+        public int? TotalItemsProcessed { get; set; }
+
+        public decimal? TotalWeightProcessed { get; set; }
+
+        public decimal? Rating { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public int? LastViewedTransportCount { get; set; }
+
+        public int? LastViewedWarehouseCount { get; set; }
+
+        public decimal? money { get; set; }
+
+        public string URL { get; set; }
+    }
+}
