6fe205ea2001b62d8f1a1b6eacce84e46f719ab4
lhs <424447025@qq.com>
2026-03-10 15:07:50 +0800

    在Model层中添加新设计的SuperAdmins实体类

---
+++ recycling.Model/SuperAdmins.cs
@@ -0,0 +1,29 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SuperAdmins
+    {
+        [Key]
+        public int SuperAdminID { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public string URL { get; set; }
+    }
+}
