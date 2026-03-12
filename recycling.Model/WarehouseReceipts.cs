9d1227415a031c5129e8002381dc4a520777572c
lhs <424447025@qq.com>
2026-03-12 14:51:32 +0800

    在Model层中添加新设计的WarehouseReceipts实体类

---
+++ recycling.Model/WarehouseReceipts.cs
@@ -0,0 +1,40 @@
+namespace recycling.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class WarehouseReceipts
+    {
+        [Key]
+        public int ReceiptID { get; set; }
+
+        [StringLength(50)]
+        public string ReceiptNumber { get; set; }
+
+        public int? TransportOrderID { get; set; }
+
+        public int? RecyclerID { get; set; }
+
+        public int? WorkerID { get; set; }
+
+        public decimal? TotalWeight { get; set; }
+
+        public string ItemCategories { get; set; }
+
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public decimal? price { get; set; }
+
+        public int? OrderID { get; set; }
+    }
+}
